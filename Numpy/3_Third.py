42697eab1ccb66363d6630ddd5df49f6ee056af9
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-02-12 18:44:21 +0530

    Add 4

--- Numpy/3_Third.py
+++ Numpy/3_Third.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 array1=np.arange(1,8)
 print(array1)
---
+++ Numpy/4_Fourth.py
@@ -0,0 +1,5 @@
+# 4
+import numpy as np
+# list=[11,22,33],[44,55,66]
+array1=np.arange(11,29).reshape(2,3,3)
+print(array1)
