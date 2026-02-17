fd7eb8f6ed493cb84f206a70b552cc5862c13187
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-02-17 23:16:46 +0530

    Create 9_Ninth.py

---
+++ Numpy/9_Ninth.py
@@ -0,0 +1,14 @@
+# 9
+import numpy as np
+array1=np.arange(1,19).reshape(2,3,3)
+array1.ndim
+print("ndim")
+print(array1)
+
+array1.shape
+print("shape")
+print(array1)
+
+array1.size
+print("size")
+print(array1)
