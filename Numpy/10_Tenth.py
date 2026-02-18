add402c3bcc3edddcbac104ae9e0716c1ff8e337
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-02-18 22:50:15 +0530

    Create 10_Tenth.py

---
+++ Numpy/10_Tenth.py
@@ -0,0 +1,9 @@
+# 9
+import numpy as np
+array1=np.array([10,20,30,40,50,60,70])
+print(array1)
+# print(array1[2])
+print(array1[1:6:2])
+print(array1[-1:-3:-1])
+print(array1[::2])
+print(array1[2::2])
