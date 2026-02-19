bbfc8cd086ea46840d3ff40140b125e5c8015ee7
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-02-19 16:21:50 +0530

    Create 11_.py

---
+++ Numpy/11_.py
@@ -0,0 +1,16 @@
+# 11
+import numpy as np
+array1=np.array([[15,16,17],[25,26,27],[35,36,37],[45,46,47]])
+print(array1)
+# print(array1[1,])
+# print(array1[3,1])
+# print(array1[1:2,1:3])
+# print(array1[0,1:3])
+# print(array1[1,0:2])
+# print(array1[2:4,1:3])
+# print(array1[1:3,0:2])
+# print(array1[0,0:2])
+print()
+print(array1[0:,0:2])
+
+# print(array1)
