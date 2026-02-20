e382894319c0d59ae6ff1d61b29d86e96555b952
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-02-20 22:57:08 +0530

    Create 12_.py

---
+++ Numpy/12_.py
@@ -0,0 +1,13 @@
+# 12
+import numpy as np
+a=np.array([[15,16,17],[25,26,27],[35,36,37],[45,46,47]])
+b=np.array([[15,16,17],[25,26,27],[35,36,37],[45,46,47]])
+
+print(f"Matrix A : \n{a}")
+print(f"Matrix B : \n{b}")
+print(f"Sum : \n{a+b}")
+print(f"sub : \n{a-b}")
+print(f"multiplication : \n{a*b}")
+print(f"div : \n{a/b}")
+print(f"floor div : \n{a//b}")
+print(f"power : \n{a**b}")
