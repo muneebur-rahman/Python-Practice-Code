f3f268a0e623f044dda21a4a4eb69753a3af2b3b
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-03-03 00:16:06 +0530

    Create 8_.py

---
+++ Tuple/8_.py
@@ -0,0 +1,10 @@
+# 8
+#convert into tupple
+l=[1,2,3]
+print(tuple(l))
+s="python"
+print(tuple(s))
+#conver dic to tuple
+d={1:"a",2:"b",3:"c"}
+print(tuple(d))  #by default key
+print(tuple(d.values())) #here value take not keys
