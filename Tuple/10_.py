a3dde183b5b8525d8d46ac2b41bb5a83938c5100
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-03-07 23:16:07 +0530

    Create 10_.py

---
+++ Tuple/10_.py
@@ -0,0 +1,9 @@
+# 10
+#traversal
+#user input and for loop tuple creation
+t=tuple()
+totalvalue=int(input("How many element u enter in tupple : "))
+for i in range(totalvalue):
+    val=int(input(f"Enter element {i} :"))
+    t=t+(val,)
+print(t)
