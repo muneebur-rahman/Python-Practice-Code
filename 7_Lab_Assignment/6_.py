4387904c4fe866a507ee26f0cce5f9fab409344e
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-03-24 16:39:25 +0530

    Create 6_.py

---
+++ 7_Lab_Assignment/6_.py
@@ -0,0 +1,11 @@
+# 6. Write a Python function to check whether a number falls within a given
+# range.
+def check_range(num, start, end):
+    if start <= num <= end:
+        return True
+    else:
+        return False
+
+# Example
+print(check_range(5, 1, 10))   # True
+print(check_range(15, 1, 10))  # False
