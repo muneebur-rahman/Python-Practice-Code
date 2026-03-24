577f1abaae72f10bff5c8bf5d008542cfdab0ab0
Muneebur Rahman <muneeburrahman0213@gmail.com>
2026-03-24 12:41:00 +0530

    Create 2_.py

---
+++ 7_Lab_Assignment/2_.py
@@ -0,0 +1,15 @@
+# 2. Write a Python function to sum all the numbers in a list.
+# Sample List : (8, 2, 3, 0, 7)
+
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+# Sample List
+sample = [8, 2, 3, 0, 7]
+
+# Function call
+result = sum_list(sample)
+print("Sum:", result)
